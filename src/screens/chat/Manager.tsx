f3e03ac1d4d2860c8dd8a9e0147777c66dd0e5dc
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-18 00:38:30 +0900

    feat: use RootContext

--- src/screens/chat/Manager.tsx
+++ src/screens/chat/Manager.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 
+import { RootContext } from '../../contexts/RootContext'
+
 export default () => {
-  const [messages, setMessages] = useState<IMessage[]>([])
+  const { messages, setMessages } = React.useContext(RootContext)
   const currentUser = {
     _id: 1,
     name: '管理人'
   }
 
   const onSend = msgs => {
-    setMessages(prev => GiftedChat.append(prev, msgs))
+    setMessages(GiftedChat.append(messages, msgs))
   }
 
   return <GiftedChat messages={messages} onSend={onSend} user={currentUser} />
