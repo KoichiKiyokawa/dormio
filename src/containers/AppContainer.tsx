33a0361a5e0b700bc08e86f0aebbb0531fdc1179
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-17 19:12:29 +0900

    feat: add Home screen

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,1 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import { Container } from 'native-base'
+import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
+
+import Home from '../screens/Home'
+import Meal from '../screens/Meal'
+
+const AppNavigator = createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: () => ({
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="md-home" color={tintColor} size={32} />
+      )
+    })
+  }
+})
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default () => {
+  const [isReady, setIsReady] = useState(false)
+
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font
+    }).then(() => {
+      setIsReady(true)
+    })
+  }, [])
+
+  if (isReady) {
+    return (
+      <Container>
+        <AppContainer />
+      </Container>
+    )
+  } else {
+    return <AppLoading />
+  }
+}
---
+++ src/screens/Meal.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Meal:React.FC<> = ()=> {
+  return ()
+
+}
+
+export default Meal
