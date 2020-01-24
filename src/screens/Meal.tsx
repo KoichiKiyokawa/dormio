0021e5ccf07ea032610e19edda17fc37096d9d99
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-24 14:45:01 +0900

    refactor: move weekEnum to mocks dir

--- src/screens/Meal.tsx
+++ src/screens/Meal.tsx
@@ -12,47 +12,9 @@ import { Col, Row, Grid } from 'react-native-easy-grid'
 
 import Nav from '../components/Nav'
 import Centerize from '../components/Centerize'
+import { weekEnum } from '../mocks/weeklyMenu'
 
 export default () => {
-  const weekEnum = [
-    {
-      name: '月',
-      breakfast: 'さつまあげ',
-      dinner: 'ハンバーグ',
-      color: { info: true }
-    },
-    {
-      name: '火',
-      breakfast: '鶏の塩焼き',
-      dinner: '生姜焼き',
-      color: { danger: true }
-    },
-    {
-      name: '水',
-      breakfast: '白身フライ',
-      dinner: 'チキングリル',
-      color: { primary: true }
-    },
-    {
-      name: '木',
-      breakfast: 'オムレツ',
-      dinner: 'ちくわの煮物',
-      color: { success: true }
-    },
-    {
-      name: '金',
-      breakfast: 'ミニ天津',
-      dinner: '魚の煮付け',
-      color: { warning: true }
-    },
-    {
-      name: '土',
-      breakfast: 'サバの南蛮漬け',
-      dinner: 'ポークカレー',
-      color: { info: true }
-    }
-  ]
-
   const rowStyle = {
     height: 40
   }
