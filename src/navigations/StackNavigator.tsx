7a8c07651f4329efb8b2452f15e6eeb66687657c
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-16 01:17:09 +0900

    feat: add SignIn and SignUp feature

--- src/navigations/StackNavigator.tsx
+++ src/navigations/StackNavigator.tsx
@@ -6,8 +6,14 @@ import NoticeDetail from '../screens/NoticeDetail'
 import Manager from '../screens/chat/Manager'
 import Residents from '../screens/chat/Residents'
 import Setting from '../screens/Setting'
+import SignIn from '../screens/SignIn'
+import SignUp from '../screens/SignUp'
+import PasswordReset from '../screens/PasswordReset'
 
 export type StackParams = {
+  SignIn: undefined
+  SignUp: undefined
+  PasswordReset: undefined
   Main: undefined
   NoticeDetail: { notice: { title: string; body: string; date: Date } }
   ManagerChat: undefined
@@ -19,6 +25,9 @@ const Stack = createStackNavigator<StackParams>()
 
 export default () => (
   <Stack.Navigator headerMode="none">
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+    <Stack.Screen name="PasswordReset" component={PasswordReset} />
     <Stack.Screen name="Main" component={TabNavigator} />
     <Stack.Screen name="NoticeDetail" component={NoticeDetail} />
     <Stack.Screen name="ManagerChat" component={Manager} />
