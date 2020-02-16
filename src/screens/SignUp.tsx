33231121f301466c78435742afb77aa5a8364c89
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-16 19:21:47 +0900

    feat: signup

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,23 +1,47 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Button, Container, Content, Header, Input, List, ListItem, Text } from 'native-base'
+import { Button, Container, Content, Header, Input, List, ListItem, Spinner, Text } from 'native-base'
 import { firebase } from '../plugins/firebase'
 import { useNavigation } from '@react-navigation/core'
+import { useDispatch } from 'react-redux'
 
 import Logo from '../components/Logo'
 import Space from '../components/Space'
+import { signIn } from '../store/user'
+import { db } from '../plugins/firebase'
 
 const SignIn = () => {
   const navigation = useNavigation()
+  const dispatch = useDispatch()
+
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
+  const [name, setName] = React.useState('')
+  const [roomNumber, setRoomNumber] = React.useState(0)
+  const [isLoading, setIsLoading] = React.useState(false)
 
   const onPressSignInButton = () => {
+    setIsLoading(true)
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(response => console.log(response))
+      .then(response => {
+        const uid = (response.user || {}).uid || ''
+        db.collection('users')
+          .add({
+            isManager: false,
+            name,
+            roomNumber,
+            uid
+          })
+          .then(response => {
+            console.log(response)
+            dispatch(signIn({ isManager: false, name, roomNumber, uid }))
+            navigation.navigate('Main')
+          })
+      })
       .catch(error => console.warn(error))
+      .finally(() => setIsLoading(false))
   }
 
   return (
@@ -42,6 +66,16 @@ const SignIn = () => {
               onChangeText={inputPassword => setPassword(inputPassword)}
             />
           </ListItem>
+          <ListItem>
+            <Input placeholder="名前" onChangeText={inputName => setName(inputName)} />
+          </ListItem>
+          <ListItem>
+            <Input
+              placeholder="部屋番号"
+              keyboardType="number-pad"
+              onChangeText={inputRoomNumber => setRoomNumber(parseInt(inputRoomNumber))}
+            />
+          </ListItem>
         </List>
         <View style={{ marginTop: 12, flexDirection: 'row', justifyContent: 'center' }}>
           <Button
@@ -49,7 +83,7 @@ const SignIn = () => {
             onPress={onPressSignInButton}
             style={{ width: '66%', justifyContent: 'center' }}
           >
-            <Text>新規登録</Text>
+            {isLoading ? <Spinner /> : <Text>新規登録</Text>}
           </Button>
         </View>
         <View style={{ marginTop: 12, flexDirection: 'column', alignItems: 'center' }}>
