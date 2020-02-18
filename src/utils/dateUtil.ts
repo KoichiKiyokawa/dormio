7db701a4d5d5578d81980596d856e15213fcbd07
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-18 23:13:30 +0900

    docs: add jsdoc comment

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -3,13 +3,23 @@ type Week = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturd
 export const mealWeekNames: Week[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 const weekNames: Week[] = ['sunday', ...mealWeekNames]
 
-export const parseToMonthWithDay = (date: Date) => {
+/**
+ * 与えられたdateをMM月DD日の形式に変換する
+ * @param {Date} date 変換したい日付
+ * @return {string} YY月DD日
+ */
+export const parseToMonthWithDay = (date: Date): string => {
   const month = date.getMonth() + 1
   const day = date.getDate()
 
   return `${month}/${day}`
 }
 
+/**
+ * 与えられたdateの曜日を返す
+ * @param {Date} date 曜日を知りたい日付
+ * @return {Week} 曜日
+ */
 export const getWeekName = (date: Date): Week => {
   const weekIndex = date.getDay()
 
