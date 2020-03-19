56e2723cd9c74fbf95e56536cc6ee601707f9ebc
K.Kiyokawa <koichi20110068@gmail.com>
2020-03-19 15:42:13 +0900

    chore: setup dotenv

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,10 +1,20 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
-import { firebaseConfig } from '../../env'
+import { config } from 'dotenv'
+config()
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({ ...firebaseConfig })
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  })
 }
 
 const db = firebase.firestore()
